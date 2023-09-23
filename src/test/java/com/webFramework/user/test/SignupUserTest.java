0e263aed47277793b523e2b1293e9a05e2068272
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-09-23 17:02:29 +0900

    test: 회원가입 기능 테스트

---
+++ src/test/java/com/webFramework/user/test/SignupUserTest.java
@@ -0,0 +1,35 @@
+package com.webFramework.user.test;
+
+import com.webFramework.domain.UserDTO;
+import com.webFramework.persistence.UserDAO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/**/root-context.xml"})
+
+public class SignupUserTest {
+    @Inject
+    UserDAO userDAO;
+
+    @Test
+    public void signupUserTest() {
+        try {
+            UserDTO userDTO = new UserDTO();
+            userDTO.setUserId("testId");
+            userDTO.setUserPassword("testPassword");
+            userDTO.setUserName("테스트이름");
+            userDTO.setUserGender("남자");
+            userDTO.setUserCallNumber("01012345678");
+            userDAO.signupUser(userDTO);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("실패");
+        }
+    }
+}
