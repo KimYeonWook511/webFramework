1a2e9c945624be39dd8ac21e1713e6ab0936ba67
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-10-10 16:09:49 +0900

    feat: 메인 페이지로 이동 구현

--- src/main/java/com/webFramework/controller/HomeController.java
+++ src/main/java/com/webFramework/controller/HomeController.java
@@ -1,13 +1,25 @@
 package com.webFramework.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String tt() {
+    public String mainGET() {
+        logger.info("mainGET 실행");
+        return "index";
+    }
+
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public String indexGET() {
+        logger.info("indexGET 실행");
         return "index";
     }
 }
