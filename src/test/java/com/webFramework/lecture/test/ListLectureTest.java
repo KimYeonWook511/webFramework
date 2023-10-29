b6839f63f8b23410327c19cd588cd47c6db0655b
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-10-29 21:44:54 +0900

    test: 대분류 기준 강의 목록 조회 기능 테스트

--- src/test/java/com/webFramework/lecture/test/ListLectureTest.java
+++ src/test/java/com/webFramework/lecture/test/ListLectureTest.java
@@ -1,6 +1,6 @@
 package com.webFramework.lecture.test;
 
-import com.webFramework.domain.LectureDTO;
+import com.webFramework.domain.LectureVO;
 import com.webFramework.service.LectureService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,27 +8,33 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/**/root-context.xml"})
 
-public class CreateLectureTest {
+public class ListLectureTest {
     @Inject
     LectureService lectureService;
 
     @Test
-    public void createLectureTest() {
-        try {
-            LectureDTO lectureDTO = new LectureDTO();
-            lectureDTO.setLectureTeacherNo(0); // 강사 회원 번호
-            lectureDTO.setLectureName("강의제목테스트"); // 강의 제목
-            lectureDTO.setLectureContent("강의내용테스트"); // 강의 내용
+    public void listLectureTest() {
+        int courseNo = 0;
+        int categoryNo = 0;
+        int skillNo = 0;
 
-            lectureService.createLecture(lectureDTO);
+        try {
+            List<LectureVO> list = lectureService.listLecture(courseNo, categoryNo, skillNo);
+
+            for (LectureVO lectureVO : list) {
+                System.out.println(lectureVO.toString());
+            }
 
         } catch (Exception e) {
             e.printStackTrace();
-            System.out.println("실패");
+            System.out.println("createCourse 실패");
         }
     }
 }
