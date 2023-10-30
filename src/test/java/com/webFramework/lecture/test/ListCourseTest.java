16b76874a092d4e48b81073bf0231b5b7d7ee9e9
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-10-30 22:49:27 +0900

    test: 대분류 목록 조회 기능 테스트

---
+++ src/test/java/com/webFramework/lecture/test/ListCourseTest.java
@@ -0,0 +1,35 @@
+package com.webFramework.lecture.test;
+
+import com.webFramework.domain.LectureVO;
+import com.webFramework.service.LectureService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/**/root-context.xml"})
+
+public class ListCourseTest {
+    @Inject
+    LectureService lectureService;
+
+    @Test
+    public void listCourseTest() {
+        try {
+            List<Map<String, Object>> list = lectureService.listCourse();
+
+            for (Map<String, Object> course : list) {
+                System.out.println("courseNo: " + course.get("courseNo") + " | courseName: " + course.get("courseName"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("createCourse 실패");
+        }
+    }
+}
