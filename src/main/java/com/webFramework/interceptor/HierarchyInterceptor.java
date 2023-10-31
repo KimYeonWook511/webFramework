b936399db41ade5d8062f446982ae4718481bc52
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-10-31 17:19:56 +0900

    feat: 강의 분류 목록 세션 인터셉터 수정
    Map<courseName, Map<categoryName, List<skillName>>>의 형태로 변경함

--- src/main/java/com/webFramework/interceptor/HierarchyInterceptor.java
+++ src/main/java/com/webFramework/interceptor/HierarchyInterceptor.java
@@ -9,6 +9,10 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HierarchyInterceptor extends HandlerInterceptorAdapter {
     private static final Logger logger = LoggerFactory.getLogger(HierarchyInterceptor.class);
@@ -19,12 +23,32 @@ public class HierarchyInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response,
                              Object handler) throws Exception {
-        logger.info("hierarchySession preHandle 실행");
+        logger.info("hierarchyInterceptor preHandle 실행");
 
         HttpSession session = request.getSession();
 
-        if (session.getAttribute("hierarchyList") == null) {
-            session.setAttribute("hierarchyList", lectureService.listSkill());
+        if (session.getAttribute("hierarchyMap") == null) {
+            List<Map<String, Object>> hierarchyList = lectureService.listSkill();
+
+            Map<String, Map<String, List<String>>> hierarchyMap = new HashMap<>();
+
+            for (Map<String,Object> hierarchy : hierarchyList) {
+                String courseName = (String)hierarchy.get("courseName");
+                String categoryName = (String)hierarchy.get("categoryName");
+                String skillName = (String)hierarchy.get("skillName");
+
+                Map<String, List<String>> innerMap = new HashMap<>();
+
+                // innerMap(Map<String, List<String>>)
+                if (hierarchyMap.containsKey(courseName)) innerMap = hierarchyMap.get(courseName);
+                else hierarchyMap.put(courseName, innerMap);
+
+                // skillName(List<String>)
+                if (!innerMap.containsKey(categoryName)) innerMap.put(categoryName, new ArrayList<>());
+                innerMap.get(categoryName).add(skillName);
+            }
+
+            session.setAttribute("hierarchyMap", hierarchyMap);
         }
 
         return true;
