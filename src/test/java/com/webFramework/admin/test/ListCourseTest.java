813d72568eba291f948cdf60d3763b6dbf0187f0
KimYeonWook511 <kimyeonwook511@gmail.com>
2023-11-10 14:49:09 +0900

    test: 대분류 마스터 테이블 리스트 조회 기능 테스트

---
+++ src/test/java/com/webFramework/admin/test/ListCourseTest.java
@@ -0,0 +1,32 @@
+package com.webFramework.admin.test;
+
+import com.webFramework.domain.CourseVO;
+import com.webFramework.service.AdminService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
+import java.util.List;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/**/root-context.xml"})
+
+public class ListCourseTest {
+    @Inject
+    AdminService adminService;
+
+    @Test
+    public void listCourseTest() {
+        try {
+            List<CourseVO> listCourse = adminService.listMasterCourse();
+
+            System.out.println(listCourse.toString());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("createCategory 실패");
+        }
+    }
+}
